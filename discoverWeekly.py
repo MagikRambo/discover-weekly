c6456ee18f72ba8dbef6a8e133a5f82dfe165e5e
mike marambio <u1229804@utah.edu>
2023-10-06 22:49:05 -0500

    utilizing spotipy instead of bare knuckle spotify api. Does heavy refresh token lifting for me

--- discoverWeekly.py
+++ discoverWeekly.py
@@ -1,137 +1,137 @@
 # import necessary modules
-import time
-import spotipy
-from dotenv import load_dotenv
-import json, os, signal
-from spotipy.oauth2 import SpotifyOAuth
-from multiprocessing import Process
 
-from flask import Flask, request, url_for, session, redirect, jsonify
+#Based implementation on https://github.com/EthanRosenthal/discovered-weekly/blob/main/discovered_weekly.py
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+import logging
+import datetime
+import sys
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
-STATE_KEY = "spotify_auth_state"
-CLIENT_ID = os.environ["CLIENT_ID"]
-CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-# initialize Flask app
-app = Flask(__name__)
+logger = logging.getLogger("discovered-weekly")
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(logging.Formatter(fmt="%(asctime)s : %(levelname)s : %(message)s"))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
-# set the name of the session cookie
-app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
+def main():
+        
+    CLIENT_ID = os.environ["CLIENT_ID"]
+    CLIENT_SECRET = os.environ["CLIENT_SECRET"]
+    REDIRECT_URI = os.environ["REDIRECT_URI"]
+    REFRESH_TOKEN = os.environ["REFRESH_TOKEN"]
+    DISCOVER_WEEKLY_ID = os.environ["DISCOVER_WEEKLY_ID"]
+    ALL_DISCOVERED_PLAYLIST_ID = os.environ["ALL_DISCOVERED_PLAYLIST_ID"]
+    USER_ID = "xmmxhd123"
+    logger.info("Start discover weekly archiving")
 
-# set a random secret key to sign the cookie
-app.secret_key = CLIENT_SECRET
+    client = load_client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, USER_ID, REFRESH_TOKEN)
 
-# set the key for the token info in the session dictionary
-TOKEN_INFO = 'token_info'
-
-
-# route to handle logging in
-@app.route('/')
-def login():
-    # create a SpotifyOAuth instance and get the authorization URL
-    auth_url = create_spotify_oauth().get_authorize_url()
-    # redirect the user to the authorization URL
-    return redirect(auth_url)
-
-@app.route('/stopServer', methods=['GET'])
-def stopServer():
-    os.kill(os.getpid(), signal.SIGINT)
-    return jsonify({ "success": True, "message": "Saved Weekly updated. Server is shutting down..." })
-
-# route to handle the redirect URI after authorization
-@app.route('/redirect')
-def redirect_page():
-    # clear the session
-    session.clear()
-    # get the authorization code from the request parameters
-    code = request.args.get('code')
-    # exchange the authorization code for an access token and refresh token
-    token_info = create_spotify_oauth().get_access_token(code)
-    # save the token info in the session
-    session[TOKEN_INFO] = token_info
-    # redirect the user to the save_discover_weekly route
-    return redirect(url_for('save_discover_weekly',_external=True))
-
-# route to save the Discover Weekly songs to a playlist
-@app.route('/saveDiscoverWeekly')
-def save_discover_weekly():
-    try: 
-        # get the token info from the session
-        token_info = get_token()
-    except:
-        # if the token info is not found, redirect the user to the login route
-        print('User not logged in')
-        return redirect("/")
-
-    # create a Spotipy instance with the access token
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    user_id = sp.current_user()['id']
-
-    # get the user's playlists
-    current_playlists =  sp.current_user_playlists()['items']
-
-    # print(current_playlists)
-    discover_weekly_playlist_id = None
-    saved_weekly_playlist_id = None
-
-    # find the Discover Weekly and Saved Weekly playlists
-    for playlist in current_playlists:
-        if(playlist['name'] == 'Discover Weekly'):
-            print('discover weekly found')
-            discover_weekly_playlist_id = playlist['id']
-        if(playlist['name'] == 'Saved Weekly'):
-            saved_weekly_playlist_id = playlist['id']
+    #parse this week's music
+    playlist_date, dw_uris = parse_current_week(client, DISCOVER_WEEKLY_ID)
     
-    # if the Discover Weekly playlist is not found, return an error message
-    if not discover_weekly_playlist_id:
-        return 'Discover Weekly not found.'
-    if not saved_weekly_playlist_id:
-        new_playlist = sp.user_playlist_create(user_id, 'Saved Weekly', public=True, collaborative=False, description="created through a python script and cron job on GHA :)")
-        saved_weekly_playlist_id = new_playlist['id']
-    
-    # get the tracks from the Discover Weekly playlist
-    discover_weekly_playlist = sp.playlist_items(discover_weekly_playlist_id)
-    song_uris = []
-    for song in discover_weekly_playlist['items']:
-        song_uri= song['track']['uri']
-        song_uris.append(song_uri)
-    
-    # add the tracks to the Saved Weekly playlist
-    sp.user_playlist_add_tracks(user_id, saved_weekly_playlist_id, song_uris, None)
+    logger.info(f"Found this week's playlist for {playlist_date}")
 
-    # return a success message
-    #print('Discover Weekly songs added successfully')
-    # return redirect('/stopServer')
-    return ('Discover Weekly songs added successfully')
+    logger.info("Adding to all time playlist")
+    add_to_all_time_playlist(client, dw_uris, ALL_DISCOVERED_PLAYLIST_ID)
+
+    logger.info("Adding to the weekly archive")
+    add_to_weekly_archive(client, USER_ID, playlist_date, dw_uris)
+
+    logger.info("Done discover weekly archiving")
 
 
-# function to get the token info from the session
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
-    if not token_info:
-        # if the token info is not found, redirect the user to the login route
-        redirect(url_for('login', _external=False))
-    
-    # check if the token is expired and refresh it if necessary
-    now = int(time.time())
+def load_client(client_id, client_secret, redirect_uri, username, refresh_token):
+    scopes = ["playlist-read-private", "playlist-modify-private"]
 
-    is_expired = token_info['expires_at'] - now < 60
-    if(is_expired):
-        spotify_oauth = create_spotify_oauth()
-        token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
-
-    return token_info
-
-def create_spotify_oauth():
-    return SpotifyOAuth(
-        client_id = CLIENT_ID,
-        client_secret = CLIENT_SECRET,
-        redirect_uri = url_for('redirect_page', _external=True),
-        scope='user-library-read playlist-modify-public playlist-modify-private'
+    #authenticate with creds
+    auth_manager = SpotifyOAuth(
+        scope=scopes,
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        username=username
     )
 
+    print(f"refresh token: {refresh_token}")
+    print(f"auth_manager: {auth_manager}")
+    auth_manager.refresh_access_token(refresh_token=refresh_token)
+    client = spotipy.Spotify(auth_manager=auth_manager)
+    return client
 
+def parse_current_week(client, discover_weekly_id):
+
+    #Discover Weekly Items and parse for info
+    dw_items = client.playlist_items(discover_weekly_id)
+    playlist_created = datetime.datetime.strptime(
+        dw_items["items"][0]["added_at"], "%Y-%m-%dT%H:%M:%Sz"
+    )
+
+    playlist_date = playlist_created.strftime("%Y-%m-%d")
+    dw_uris = [item["track"]["uri"] for item in dw_items["items"]]
+    return playlist_date, dw_uris
+
+def add_to_all_time_playlist(client, dw_uris, all_discovered_playlist_id):
+    # First, add to the all time DW
+
+    # Determine total number of tracks
+    total = client.playlist(all_discovered_playlist_id)["tracks"]["total"]
+    # Now, query for the last 5 tracks
+    offset = max(0, total - 5)
+    last_five = client.playlist_items(all_discovered_playlist_id, offset=offset)
+    # If the last 5 tracks match the last 5 from the current week, then we've already added
+    # this week's playlist.
+    match = len(last_five["items"]) >= 5 and all(
+        [
+            dw_uri == item["track"]["uri"]
+            for dw_uri, item in zip(dw_uris[-5:], last_five["items"])
+        ]
+    )
+    if match:
+        logger.info(
+            "This script has already been run for this week."
+            " Skipping add to all time playlist."
+        )
+        return
+
+    client.playlist_add_items(all_discovered_playlist_id, dw_uris)
+
+def add_to_weekly_archive(client, username, playlist_date, dw_uris):
+    # Second, create the weekly archive playlist
+    this_weeks_playlist = f"Discovered Week {playlist_date}"
+
+    # Need to search all user's playlists to see if this one already exists...
+    limit = 50
+    offset = 0
+    total = 1e9
+    found = False
+
+    while offset + limit < total and not found:
+        playlists = client.user_playlists(username, limit=limit, offset=offset)
+        total = playlists["total"]
+        found = any(
+            [item["name"] == this_weeks_playlist for item in playlists["items"]]
+        )
+        offset += limit
+
+    if found:
+        logger.info(
+            "This script has already been run for this week."
+            " Skipping creation of weekly archive playlist."
+        )
+        return
+
+    logger.info(f"Creating this week's archive playlist: {this_weeks_playlist}")
+    saved_playlist = client.user_playlist_create(
+        username, this_weeks_playlist, public=False
+    )
+    client.playlist_add_items(saved_playlist["id"], dw_uris)
+    logger.info("Done creating this week's archive playlist.")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    main()
